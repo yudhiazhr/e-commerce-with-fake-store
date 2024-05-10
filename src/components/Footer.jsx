b6047e2c885208cceaccd7fd5701d0359690125c
yudhiazhr <m.yudhiazhari@gmail.com>
2024-05-10 08:27:07 +0700

    feat: add footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,16 @@
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="bg-primary py-12">
+        <div className="container mx-auto">
+          <p className="text-white text-center">
+            Copyright &copy; Ecommerce Shop with FakeStore API 2024.<br/> Made with Love <span className="text-red-500">❤</span> 
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
